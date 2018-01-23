763f1fe1db09eb1b9ff364d6029155b07ac52291
HaipengWang <haip_wang@163.com>
2018-01-22 22:46:37 -0800

    use different key for url

--- src/loaders/cdn-loader.js
+++ src/loaders/cdn-loader.js
@@ -2,12 +2,12 @@ const rp = require('request-promise-native');
 const _ = require('lodash');
 
 module.exports = function cdnBuilder() {
-  return (source, { query, metadata: { urls }}) => {
-    let output = 'release';
+  return (source, { query, metadata: { url, urlDebug = url }}) => {
+    let requestUrl = url;
     if (_.has(query, 'debug')) {
-      output = 'debug';
+      requestUrl = urlDebug;
     }
 
-    return rp.get(urls[output]);
+    return rp.get(requestUrl);
   }
 };
