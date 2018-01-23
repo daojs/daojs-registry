03f728b048cc6ceda67ff44066440dbf9405e00f
Wei Wei <shazhouke@gmail.com>
2018-01-23 16:04:27 +0800

    Fix eslint

--- src/loaders/cdn-loader.js
+++ src/loaders/cdn-loader.js
@@ -2,12 +2,12 @@ const rp = require('request-promise-native');
 const _ = require('lodash');
 
 module.exports = function cdnBuilder() {
-  return (source, { query, metadata: { url, urlDebug = url }}) => {
+  return (source, { query, metadata: { url, urlDebug = url } }) => {
     let requestUrl = url;
     if (_.has(query, 'debug')) {
       requestUrl = urlDebug;
     }
 
     return rp.get(requestUrl);
-  }
+  };
 };
