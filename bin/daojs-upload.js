fd4bdfb1dd50740c60a1f2f87534e53c11446da4
FAREAST\yuzhon <yuzhon@microsoft.com>
2018-01-27 22:40:25 +0800

    add esprima, generate dependencies from source code

--- bin/daojs-upload.js
+++ bin/daojs-upload.js
@@ -1,34 +1,64 @@
-/* eslint-disable */
 const Promise = require('bluebird');
 const fs = Promise.promisifyAll(require('fs'));
 const axios = require('axios');
+const esprima = require('esprima');
+const _ = require('lodash');
 
 const { argv } = require('yargs')
-  .option('name', { alias: 'n', default: '' })
-  .option('type', { alias: 't', default: 'es2015' })
-  .option('desc', { alias: 'd', default: '' })
+  .option('metadata', { alias: 'm', default: 'daopkg.json' })
   .option('source', { alias: 'sm', default: 'source.min.js' })
   .option('sourceDebug', { alias: 's', default: 'source.js' })
   .option('readme', { alias: 'r', default: 'readme.md' });
 
 const url = 'http://daojs.koreasouth.cloudapp.azure.com/resources/';
 
-Promise.map([argv.source, argv.sourceDebug, argv.readme], path => fs.readFileAsync(path))
-  .then(files => axios.post(`${url}${argv.name}`, {
-    source: {
-      data: files[0],
-    },
-    sourceDebug: {
-      data: files[1],
-    },
-    readme: {
-      data: files[2],
-    },
-    type: argv.type,
-  }))
-  .then(response => {
+Promise.map([argv.metadata, argv.source, argv.sourceDebug, argv.readme], path =>
+  fs.readFileAsync(path))
+  .then((files) => {
+    const {
+      name,
+      type,
+      description,
+      dependencies,
+    } = JSON.parse(files[0]);
+
+    return axios.post(`${url}${name}`, {
+      source: {
+        data: files[0],
+      },
+      sourceDebug: {
+        data: files[1],
+      },
+      readme: {
+        data: files[2],
+      },
+      type,
+      description,
+      dependencies: _.chain(esprima.parseModule(files[2], { jsx: true }).body)
+        .filter(syntaxTreeNode => syntaxTreeNode.type === 'ImportDeclaration')
+        .reduce((deps, importDeclaration) => {
+          const importFrom = importDeclaration.source.value;
+          return _.defaults({
+            [importFrom]: {
+              version: dependencies[importFrom] || 0,
+              variable: ((specifiers) => {
+                if (_.size(specifiers) === 1 &&
+                    _.first(specifiers).type === 'ImportDefaultSpecifier') {
+                  return _.first(specifiers).local.name;
+                }
+                return _.reduce(specifiers, (result, specifier) => _.defaults({
+                  [specifier.local.name]: specifier.local.name,
+                }, result), {});
+              })(importDeclaration.specifiers),
+            },
+          }, deps);
+        }, {})
+        .value(),
+    });
+  })
+  .then((response) => {
     console.log(response);
   })
-  .catch(error => {
+  .catch((error) => {
     console.log(error);
   });
