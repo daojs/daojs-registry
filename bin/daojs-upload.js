03b2c6f2ba0fbb781b2b35805b5b7fd84ab87ba5
FAREAST\yuzhon <yuzhon@microsoft.com>
2018-01-28 17:03:10 +0800

    log verbose

--- bin/daojs-upload.js
+++ bin/daojs-upload.js
@@ -5,38 +5,70 @@ const axios = require('axios');
 const esprima = require('esprima');
 const _ = require('lodash');
 const chalk = require('chalk');
+const path = require('path');
 
 const { argv } = require('yargs')
+  .option('path', { alias: 'p', default: './' })
   .option('metadata', { alias: 'm', default: 'daopkg.json' })
-  .option('source', { alias: 'sm', default: 'source.min.js' })
-  .option('sourceDebug', { alias: 's', default: 'source.js' })
-  .option('readme', { alias: 'r', default: 'readme.md' });
+  .option('source', { alias: 's', default: 'source.js' })
+  .option('sourceMin', { alias: 'sm', default: 'source.min.js' })
+  .option('readme', { alias: 'r', default: 'readme.md' })
+  .option('verbose', { alias: 'v' });
 
-const url = 'http://daojs.koreasouth.cloudapp.azure.com/resources/';
+const isVerbose = argv.verbose;
 
-Promise.map([argv.metadata, argv.source, argv.sourceDebug, argv.readme], path =>
-  fs.readFileAsync(path, 'utf8'))
-  .then((files) => {
+const log = (...args) => console.log(...args);
+log.succ = (...args) => log(chalk.green(...args));
+log.error = (...args) => log(chalk.red(...args));
+log.verbose = (...args) => isVerbose && log(...args);
+
+const url = 'http://daojs.koreasouth.cloudapp.azure.com';
+
+axios.interceptors.request.use((config) => {
+  // Log request info before sent
+  log.verbose('Request:', config.method.toUpperCase(), config.url);
+  log.verbose('Request payload:', JSON.stringify(config.data));
+  return config;
+});
+
+Promise.props(_.mapValues({
+  metadata: argv.metadata,
+  source: argv.source,
+  sourceMin: argv.sourceMin,
+  readme: argv.readme,
+}, (value, key) => {
+  const encoding = 'utf8';
+  const filePath = path.resolve(argv.path, value);
+
+  log.verbose('Read', key, 'from', filePath, 'with encoding', encoding);
+  return fs.readFileAsync(filePath, encoding);
+}))
+  .then(({
+    metadata,
+    sourceMin,
+    source,
+    readme,
+  }) => {
     const {
-      name,
-      type,
-      description,
-      dependencies,
-    } = JSON.parse(files[0]);
+      name = '',
+      type = 'es2015',
+      description = '',
+      dependencies = {},
+    } = JSON.parse(metadata);
 
-    return axios.post(`${url}${name}`, {
+    return axios.post(`${url}/resources/${name}`, {
       source: {
-        data: files[1],
+        data: sourceMin,
       },
       sourceDebug: {
-        data: files[2],
+        data: source,
       },
       readme: {
-        data: files[3],
+        data: readme,
       },
       type,
       description,
-      dependencies: _.chain(esprima.parseModule(files[2], { jsx: true }).body)
+      dependencies: _.chain(esprima.parseModule(source, { jsx: true }).body)
         .filter(syntaxTreeNode => syntaxTreeNode.type === 'ImportDeclaration')
         .reduce((deps, importDeclaration) => {
           const importFrom = importDeclaration.source.value;
@@ -59,8 +91,12 @@ Promise.map([argv.metadata, argv.source, argv.sourceDebug, argv.readme], path =>
     });
   })
   .then((response) => {
-    console.log(chalk.green('SUCCES'), 'version =', response.data.version);
+    log.succ('DONE version =', response.data.version);
+    log('View your component at', response.config.url.replace(`${url}/resources/`, `${url}/public/#/detail/`));
   })
   .catch((error) => {
-    console.log(chalk.red('FAILED'), error);
+    log.error(error);
+    if (error.response) {
+      log.error('Response:', _.get(error, 'response.data.message', 'empty'));
+    }
   });
