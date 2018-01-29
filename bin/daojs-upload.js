06b952aed997d531f8d8581967f3bf70ec050add
FAREAST\yuzhon <yuzhon@microsoft.com>
2018-01-29 20:21:52 +0800

    Recursively upload all sub directories

--- bin/daojs-upload.js
+++ bin/daojs-upload.js
@@ -7,22 +7,19 @@ const _ = require('lodash');
 const chalk = require('chalk');
 const path = require('path');
 
+const url = 'http://localhost:3000';
+
 const { argv } = require('yargs')
   .option('path', { alias: 'p', default: './' })
-  .option('metadata', { alias: 'm', default: 'daopkg.json' })
+  .option('metadata', { alias: 'm', default: 'metadata.json' })
   .option('source', { alias: 's', default: 'source.js' })
-  .option('sourceMin', { alias: 'sm', default: 'source.min.js' })
   .option('readme', { alias: 'r', default: 'readme.md' })
   .option('verbose', { alias: 'v' });
-
-const isVerbose = argv.verbose;
 
 const log = (...args) => console.log(...args);
 log.succ = (...args) => log(chalk.green(...args));
 log.error = (...args) => log(chalk.red(...args));
-log.verbose = (...args) => isVerbose && log(...args);
-
-const url = 'http://daojs.koreasouth.cloudapp.azure.com';
+log.verbose = (...args) => argv.verbose && log(...args);
 
 axios.interceptors.request.use((config) => {
   // Log request info before sent
@@ -31,72 +28,91 @@ axios.interceptors.request.use((config) => {
   return config;
 });
 
-Promise.props(_.mapValues({
-  metadata: argv.metadata,
-  source: argv.source,
-  sourceMin: argv.sourceMin,
-  readme: argv.readme,
-}, (value, key) => {
-  const encoding = 'utf8';
-  const filePath = path.resolve(argv.path, value);
+const isDirectory = source => fs.lstatAsync(source).then(stat => stat.isDirectory());
+const getDirectories = source => fs.readdirAsync(source)
+  .map(name => path.join(source, name))
+  .filter(isDirectory);
 
-  log.verbose('Read', key, 'from', filePath, 'with encoding', encoding);
-  return fs.readFileAsync(filePath, encoding);
-}))
-  .then(({
-    metadata,
-    sourceMin,
-    source,
-    readme,
-  }) => {
-    const {
-      name = '',
-      type = 'es2015',
-      description = '',
-      dependencies = {},
-    } = JSON.parse(metadata);
+let rootName;
 
-    return axios.post(`${url}/resources/${name}`, {
-      source: {
-        data: sourceMin,
-      },
-      sourceDebug: {
-        data: source,
-      },
-      readme: {
-        data: readme,
-      },
-      type,
-      description,
-      dependencies: _.chain(esprima.parseModule(source, { jsx: true }).body)
-        .filter(syntaxTreeNode => syntaxTreeNode.type === 'ImportDeclaration')
-        .reduce((deps, importDeclaration) => {
-          const importFrom = importDeclaration.source.value;
-          return _.defaults({
-            [importFrom]: {
-              version: dependencies[importFrom] || 0,
-              variable: ((specifiers) => {
-                if (_.size(specifiers) === 1 &&
-                    _.first(specifiers).type === 'ImportDefaultSpecifier') {
-                  return _.first(specifiers).local.name;
-                }
-                return _.reduce(specifiers, (result, specifier) => _.defaults({
-                  [specifier.local.name]: specifier.local.name,
-                }, result), {});
-              })(importDeclaration.specifiers),
-            },
-          }, deps);
-        }, {})
-        .value(),
+function upload(directory, isRoot = false) {
+  let currentName;
+
+  // Recursively upload all sub directories
+  const subTasks = getDirectories.map(upload);
+
+  // Upload current directory
+  const currentTask = Promise.props(_.mapValues({
+    metadata: argv.metadata,
+    source: argv.source,
+    readme: argv.readme,
+  }, (fileName, fileType) => {
+    const encoding = 'utf8';
+    const filePath = path.resolve(directory, fileName);
+
+    log.verbose('Read', fileType, 'from', filePath, 'with encoding', encoding);
+    return fs.readFileAsync(filePath, encoding);
+  }))
+    .then(({
+      metadata,
+      source,
+      readme,
+    }) => {
+      const {
+        name = '',
+        type = 'es2015',
+        description = '',
+        dependencies = {},
+      } = JSON.parse(metadata);
+      if (isRoot) {
+        if (!name.startsWith('@/')) {
+          throw new Error('Component name must starts with @/');
+        }
+        rootName = name;
+      }
+      currentName = rootName + path.relative(argv.path, directory);
+
+      return axios.post(`${url}/resources/${currentName}`, {
+        source,
+        readme,
+        type,
+        description,
+        dependencies: _.chain(esprima.parseModule(source, { jsx: true }).body)
+          .filter(syntaxTreeNode => syntaxTreeNode.type === 'ImportDeclaration')
+          .reduce((deps, importDeclaration) => {
+            const importFrom = importDeclaration.source.value;
+            let version = dependencies[importFrom];
+            if (_.isEmpty(version)) {
+              version = importFrom.startsWith('@/') ? '0' : '*';
+            }
+            return _.defaults({
+              [importFrom]: version,
+            }, deps);
+          }, {})
+          .value(),
+      });
+    })
+    .then((response) => {
+      log.succ('Upload successfully, name =', currentName, ' version =', response.data.version);
+      log('View your component at', response.config.url.replace(`${url}/resources/`, `${url}/public/#/detail/`));
+      return 'success';
+    })
+    .catch((error) => {
+      log.error(error);
+      if (error.response) {
+        log.error('Response:', _.get(error, 'response.data.message', 'empty'));
+      }
+      return 'failed';
     });
-  })
-  .then((response) => {
-    log.succ('DONE version =', response.data.version);
-    log('View your component at', response.config.url.replace(`${url}/resources/`, `${url}/public/#/detail/`));
-  })
-  .catch((error) => {
-    log.error(error);
-    if (error.response) {
-      log.error('Response:', _.get(error, 'response.data.message', 'empty'));
-    }
-  });
+
+  return Promise.reduce([...subTasks, currentTask], (result, status) => _.defaults({
+    [status]: (result[status] || 0) + 1,
+  }, result), {});
+}
+
+upload(argv.path, true).then((result) => {
+  log('Upload finished');
+  log('Total:', result.success + result.failed);
+  log('Success:', result.success);
+  log('Failed:', result.failed);
+});
