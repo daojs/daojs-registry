3bbc2c449a3247828b38e1637a510200f576b00f
Wei Wei <shazhouke@gmail.com>
2018-01-31 16:20:52 +0800

    Add wait time for requirejs

--- rom/@/bootloader/index.js
+++ rom/@/bootloader/index.js
@@ -4,6 +4,7 @@ var version=document.currentScript.dataset.version;
 requirejs.config({
   baseUrl: document.currentScript.src.replace(/\/@\/bootloader.js/, ''),
   urlArgs: 'v=' + version,
+  waitSeconds: 30,
   map: {
     '*': {
       css: 'require-css',
