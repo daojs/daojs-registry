2b70b3143133fd042196c67019b4ee18702568e0
Yu Zhong <yuzhon@microsoft.com>
2018-04-22 20:27:03 +0800

    load external js from cdn instead of server

--- rom/@/bootloader/index.js
+++ rom/@/bootloader/index.js
@@ -1,14 +1,40 @@
-var component=document.currentScript.dataset.component;
-var version=document.currentScript.dataset.version;
+(function() {
+  var component=document.currentScript.dataset.component;
+  var version=document.currentScript.dataset.version;
+  
+  var notAMD = [
+    'element-resize-event',
+  ];
 
-requirejs.config({
-  baseUrl: document.currentScript.src.replace(/\/@\/bootloader.js/, ''),
-  urlArgs: 'v=' + version,
-  waitSeconds: 30,
-  map: {
-    '*': {
-      css: 'require-css',
+  var paths = {
+    antd: 'https://cdnjs.cloudflare.com/ajax/libs/antd/3.1.6/antd.min',
+    echarts: 'https://cdnjs.cloudflare.com/ajax/libs/echarts/4.0.4/echarts.min',
+    'element-resize-event': 'https://cdn.jsdelivr.net/npm/element-resize-event@2.0.9/index.min',
+    lodash: 'https://cdnjs.cloudflare.com/ajax/libs/lodash.js/4.17.5/lodash.min',
+    moment: 'https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.20.1/moment.min',
+    'prop-types': 'https://cdnjs.cloudflare.com/ajax/libs/prop-types/15.6.1/prop-types.min',
+    react: 'https://cdnjs.cloudflare.com/ajax/libs/react/16.2.0/umd/react.production.min',
+    'react-dom': 'https://cdnjs.cloudflare.com/ajax/libs/react-dom/16.2.0/umd/react-dom.production.min',
+  };
+
+  requirejs.config({
+    baseUrl: document.currentScript.src.replace(/\/@\/bootloader.js/, ''),
+    urlArgs: 'v=' + version,
+    waitSeconds: 30,
+    map: {
+      '*': {
+        css: 'require-css',
+      }
+    },
+    paths: paths,
+    cajon: {
+      useXhr: function (url, protocal, hostname, port) {
+        return notAMD.reduce(function (memo, current) {
+          return memo || (url.indexOf(paths[current]) > -1);
+        }, false);
+      },
     }
-  }
-});
-require([component]);
+  });
+
+  require([component]);
+})();
