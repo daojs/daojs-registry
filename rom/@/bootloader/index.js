46604f62338781c0383fef3d9afbf43ff15f2098
suro1995 <809875346@qq.com>
2018-04-24 05:05:46 +0000

    antd css

--- rom/@/bootloader/index.js
+++ rom/@/bootloader/index.js
@@ -8,7 +8,7 @@
 
   var paths = {
     antd: 'https://cdnjs.cloudflare.com/ajax/libs/antd/3.1.6/antd.min',
-    'antd/dist/antd.css': 'https://cdnjs.cloudflare.com/ajax/libs/antd/3.4.3/antd.css',
+    'antd.css': 'https://cdnjs.cloudflare.com/ajax/libs/antd/3.4.3/antd.css',
     echarts: 'https://cdnjs.cloudflare.com/ajax/libs/echarts/4.0.4/echarts.min',
     'element-resize-event': 'https://cdn.jsdelivr.net/npm/element-resize-event@2.0.9/index.min',
     lodash: 'https://cdnjs.cloudflare.com/ajax/libs/lodash.js/4.17.5/lodash.min',
@@ -16,6 +16,7 @@
     'prop-types': 'https://cdnjs.cloudflare.com/ajax/libs/prop-types/15.6.1/prop-types.min',
     react: 'https://cdnjs.cloudflare.com/ajax/libs/react/16.2.0/umd/react.production.min',
     'react-dom': 'https://cdnjs.cloudflare.com/ajax/libs/react-dom/16.2.0/umd/react-dom.production.min',
+    'require-css': 'https://cdnjs.cloudflare.com/ajax/libs/require-css/0.1.10/css',
   };
 
   requirejs.config({
